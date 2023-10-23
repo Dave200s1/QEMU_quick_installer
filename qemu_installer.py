99e7422c08555823cdbade22955032047b9fcaaf
David <kesuke_tamaguchie_44@tutanota.com>
2023-10-23 12:34:08 +0000

    small changes

--- qemu_installer.py
+++ qemu_installer.py
@@ -49,7 +49,7 @@ class Script():
         subprocess.run(cmd,shell=True)
 
 
-    def download_iso(self):
+    def download_iso_deb(self):
         #current_dir = os.getcwd()
         cmd ="sudo wget https://cdimage.debian.org/cdimage/release/current-live/amd64/iso-hybrid/debian-live-12.2.0-amd64-xfce.iso"
         
@@ -99,21 +99,76 @@ class Script():
 
         #Remove temp_file
         subprocess.call(["rm","temp.txt"])
+        
+        
+    def ask_version(self):
+        response = input("\Which version do you want to install? (Debian/Ubuntu/Fedora): ")
+        if response.lower() == "debian":
+            self.download_iso_deb()
+        elif response.lower()== "ubuntu":
+            self.download_iso_ubu()
+        elif response.lower() == "fedora":
+            self.download_iso_fed()
+        else:
+            print("\nInvalid response, please try again...")
+            self.ask_version()
+            
+    def download_iso_fed(self):
+        #current_dir = os.getcwd()
+        cmd ="sudo wget https://download.fedoraproject.org/pub/fedora/linux/releases/38/Spins/x86_64/iso/Fedora-LXQt-Live-x86_64-38-1.6.iso"
+        
+       
+        new_dir = "/ISOs"
+        os.chdir(new_dir)
+        subprocess.run("pwd",shell=True)
+        
+        
+        subprocess.run(cmd,shell=True)
+    
+    
+    def download_iso_ubu(self):
+        #current_dir = os.getcwd()
+        cmd ="sudo wget http://ftp.uni-kl.de/pub/linux/ubuntu-dvd/xubuntu/releases/22.04/release/xubuntu-22.04.3-desktop-amd64.iso"
+        
+       
+        new_dir = "/ISOs"
+        os.chdir(new_dir)
+        subprocess.run("pwd",shell=True)
+        
+        
+        subprocess.run(cmd,shell=True)
+            
+    def check_ISO(self):
+        response = input("\Do you have installed the 'ISO' ? (y/n): ")
+        if response == "n":
+            self.ask_version()
+            self.create_VM()
+        elif response == "y":
+            self.create_VM()
+        else:
+            print("\Invalid response: Please try again..")
+            self.check_ISO()
+
+    def create_VM(self):
+        name = input("Enter the name for the VM: ")
+        location = input("Enter the location for the VM: ")
+        location_ISO = input("Enter the location of ISO: ")
+        ram = input("Enter the RAM size (in MB): ")
+        vcpu = input("Enter the number of vCPUs: ")
+        size = input("Enter the size of the disk (in GB): ")
+        dp = input("Enter the diskpath_name:(eample debian) ")
+
+        command = f"sudo virt-install --name {name} --os-type linux --os-variant unknown --ram {ram} --vcpu {vcpu} --disk path={location}{dp}.qcow2,size={size} --graphics vnc,listen=0.0.0.0 --noautoconsole --hvm --cdrom {location_ISO} --boot cdrom,hd"
+    
+        subprocess.run(command,shell=True)
+
 
     def config_bidge(self):
         print("\nPlease go to nano /etc/network/interfaces and enter the following...")
         print("\n inface br0 inet static , leave the rest and add at the bottom bridge_ports (yourInterfaceName)")
         print("\nAfter your done do a reboot and us ip a s br0 to check config")
 
-    def create_VM(self):
-        name = input("Enter the name for the VM: ")
-        ram = input("Enter the RAM size (in MB): ")
-        vcpu = input("Enter the number of vCPUs: ")
-        size = input("Enter the size of the disk (in GB): ")
-        dp = input("Enter the diskpath_name (example: debian): ")
-        command = f"sudo virt-install --name {name} --os-type linux --os-variant unknown --ram {ram} --vcpu {vcpu} --disk path=/home/dsi/VMs/{dp}.qcow2,size={size} --graphics vnc,listen=0.0.0.0 --noautoconsole --hvm --cdrom /home/dsi/ISOs/debian-live-12.2.0-amd64-xfce.iso --boot cdrom,hd --force --check all=off"
-    
-        subprocess.run(command,shell=True)
+
 
     def display_the_port(self):
         cmd = "sudo virsh list --all"
@@ -139,8 +194,7 @@ class Script():
         #self.add_VHostNet_module()
         self.check_for_vHost()
         #DO Not USE self.config_bidge()
-        #self.download_iso()
-        self.create_VM()
+        self.check_ISO()
         self.display_the_port()
         print("\nAll done ....")
         
